5bed24192e2e9424f7d9500d014bbc4950d55f32
Owais Khan <kowais915@gmail.com>
2020-08-23 10:16:17 +0500

    Playing with route and views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Owais Khan
                 </div>
 
                 <div class="links">
