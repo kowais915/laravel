93dc08caa468bb76966fb641167fe28c73701611
Owais Khan <kowais915@gmail.com>
2020-08-30 12:31:45 +0500

    Some changes to the nav men via app, and custom poges

--- resources/views/starting_page.blade.php
+++ resources/views/starting_page.blade.php
@@ -5,16 +5,11 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Starting Page') }}</div>
+                <div class="card-header">Welcome to Owpus</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                Why do we use it?
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
                 </div>
             </div>
         </div>
